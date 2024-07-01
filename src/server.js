8b26c5baa26cc902afe79ad0a913fcedfd46ddd6
KateMassa <catherineorlova27@gmail.com>
2024-07-01 20:13:29 +0200

    ++

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ import router from './routers/index.js';
 
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -18,6 +20,8 @@ export const startServer = () => {
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     pino({
