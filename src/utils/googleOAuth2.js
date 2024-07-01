ebd021afe723c237cc6e678aefc10b77d8ce8383
KateMassa <catherineorlova27@gmail.com>
2024-07-01 16:34:40 +0200

    Update googleOAuth2.js

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -19,8 +19,8 @@ const googleOAuthClient = new OAuth2Client({
 export const generateAuthUrl = () =>
   googleOAuthClient.generateAuthUrl({
     scope: [
-      '<https://www.googleapis.com/auth/userinfo.email>',
-      '<https://www.googleapis.com/auth/userinfo.profile>',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
     ],
   });
 
